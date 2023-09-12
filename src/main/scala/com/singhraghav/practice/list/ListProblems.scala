7f5dc67f723f952fcafbb1e651154b596037f06f
raghavsingh31 <raghavsingh31@outlook.com>
2023-09-12 22:35:05 +0530

    implemented length, reverse, concat for scala RList

--- src/main/scala/com/singhraghav/practice/list/ListProblems.scala
+++ src/main/scala/com/singhraghav/practice/list/ListProblems.scala
@@ -13,6 +13,13 @@ sealed abstract class RList[+T] {
 
   def ::[R >: T](element: R): RList[R] = new ::(element, this)
 
+  def apply(index: Int): T
+
+  def length: Int
+
+  def reverse: RList[T]
+
+  def ++[S >: T](other: RList[S]): RList[S]
 }
 
 case object RNil extends RList[Nothing] {
@@ -26,6 +33,13 @@ case object RNil extends RList[Nothing] {
 
   override def toString: String = "[]"
 
+  override def apply(index: Int): Nothing = throw new NoSuchElementException()
+
+  override def length: Int = 0
+
+  override def reverse: RList[Nothing] = this
+
+  override def ++[S >: Nothing](other: RList[S]): RList[S] = other
 }
 
 case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T] {
@@ -45,11 +59,60 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
     "[" + toStringTailRec(this, "") + "]"
   }
 
+  override def apply(index: Int): T = {
+    @tailrec
+    def loop(currIndex: Int, remainingList: RList[T]): T =
+      if(currIndex < index && remainingList.tail.isEmpty) throw new NoSuchElementException()
+      else if(currIndex == index) remainingList.head
+      else loop(currIndex + 1, remainingList.tail)
+
+    if (index < 0) throw new IllegalArgumentException()
+    else loop(0, this)
+  }
+
+  override def length: Int = {
+    @tailrec
+    def loop(remaining: RList[T], size: Int): Int =
+      if(remaining.isEmpty) size
+      else loop(remaining.tail, size + 1)
+
+    loop(this, 0)
+  }
+
+  override def reverse: RList[T] = {
+
+    @tailrec
+    def loop(remaining: RList[T], result: RList[T]): RList[T] =
+      if(remaining.isEmpty) result
+      else loop(remaining.tail, remaining.head :: result)
+
+    loop(this, RNil)
+  }
+
+  override def ++[S >: T](other: RList[S]): RList[S] = {
+
+    @tailrec
+    def loop(remaining: RList[S], result: RList[S]): RList[S] =
+      if(remaining.isEmpty) result
+      else loop(remaining.tail, remaining.head :: result)
+
+    loop(this.reverse, other)
+  }
 }
+
 
 object ListProblems extends App {
 
   val list1 = ::(1, ::(2, ::(3, RNil)))
 
-  println(list1)
+  println(list1(0))
+  println(list1(2))
+
+//  println(list1(3))
+
+  println(list1.reverse)
+
+  val list2 = ::(4, ::(5, RNil))
+
+  println(list2 ++ RNil)
 }
