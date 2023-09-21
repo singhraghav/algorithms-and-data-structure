4dd0c28eac979bb67a4039f2456883864a80cd1a
raghavsingh31 <raghavsingh31@outlook.com>
2023-09-21 21:08:13 +0530

    implemented insertion sort and merge sort

--- src/main/scala/com/singhraghav/practice/ds/list/ListProblems.scala
+++ src/main/scala/com/singhraghav/practice/ds/list/ListProblems.scala
@@ -35,7 +35,10 @@ sealed abstract class RList[+T] {
 
   def rotateLeft(n: Int): RList[T]
 
-  def sample(k: Int): RList[T]
+  def insertionSort[T1 >: T](ordering: Ordering[T1]): RList[T1]
+
+  def mergeSort[T1 >: T](ordering: Ordering[T1]): RList[T1]
+
 }
 
 case object RNil extends RList[Nothing] {
@@ -71,6 +74,9 @@ case object RNil extends RList[Nothing] {
 
   override def rotateLeft(n: Int): RList[Nothing] = this
 
+  override def insertionSort[T1 >: Nothing](ordering: Ordering[T1]): RList[T1] = this
+
+  override def mergeSort[T1 >: Nothing](ordering: Ordering[T1]): RList[T1] = this
 }
 
 case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T] {
@@ -213,6 +219,55 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
     if (actualRotationNeeded == 0) this else loop(RNil, this, actualRotationNeeded)
   }
 
+  override def insertionSort[T1 >: T](ordering: Ordering[T1]): RList[T1] =  {
+
+    def insertedSorted(element: T, sortedList: RList[T1]): RList[T1] = {
+
+      @tailrec
+      def loop(acc: RList[T1], remaining: RList[T1]): RList[T1] = remaining match {
+        case RNil if acc.isEmpty => element :: remaining
+        case RNil                => (element :: acc).reverse
+        case ::(head, tail)      =>
+          if (ordering.lt(head, element)) loop(head :: acc, tail)
+          else acc.reverse ++ (element :: remaining)
+      }
+
+      loop(RNil, sortedList)
+    }
+
+    @tailrec
+    def loop(acc: RList[T1], remaining: RList[T]): RList[T1] =
+      if (remaining.isEmpty) acc else loop(insertedSorted(remaining.head, acc), remaining.tail)
+
+    loop(RNil, this)
+  }
+
+  override def mergeSort[T1 >: T](ordering: Ordering[T1]): RList[T1] =  {
+
+    @tailrec
+    def merge(sortedList1: RList[T1], sortedList2: RList[T1], result: RList[T1]): RList[T1] = {
+      if(sortedList1.isEmpty && sortedList2.isEmpty) result.reverse
+      else if(sortedList1.isEmpty) result.reverse ++ sortedList2
+      else if(sortedList2.isEmpty) result.reverse ++ sortedList1
+      else {
+        if(ordering.lt(sortedList1.head, sortedList2.head)) merge(sortedList1.tail, sortedList2, sortedList1.head :: result)
+        else if(ordering.gt(sortedList1.head, sortedList2.head)) merge(sortedList1, sortedList2.tail, sortedList2.head :: result)
+        else merge(sortedList1.tail, sortedList2.tail, sortedList1.head :: sortedList2.head :: result)
+      }
+    }
+
+    @tailrec
+    def loop(smallerChunks: RList[RList[T1]], mergedChunks: RList[RList[T1]]): RList[T1] =
+      (smallerChunks, mergedChunks) match {
+        case (RNil, RNil) => RNil
+        case (RNil, mergedListHead :: mergedListTail) if mergedListTail.isEmpty  => mergedListHead
+        case (RNil, _              :: mergedListTail) if !mergedListTail.isEmpty => loop(mergedChunks, RNil)
+        case (smallerListHead :: smallerListTail, mergedChunks) if smallerListTail.isEmpty => loop(RNil, smallerListHead :: mergedChunks)
+        case (smallerListElement1 :: smallerListElement2 :: remainingSmallerList, mergedChunks) => loop(remainingSmallerList, merge(smallerListElement1, smallerListElement2, RNil) :: mergedChunks)
+      }
+
+    loop(this.map(_ :: RNil), RNil)
+  }
 }
 
 
@@ -257,4 +312,12 @@ object ListProblems extends App {
   println(list1.duplicateEach(1))
 
   println(::(1, RNil).rotateLeft(3))
+
+  val list4 = ::(9, ::(2, ::(1, ::(7, ::(4, ::(9, RNil))))))
+
+  println(list4.insertionSort(Ordering[Int]))
+
+  val list5 = 10 :: 1 :: 9 :: 2 :: 8 :: 3 :: 7 :: 4 :: 5 :: 5 :: 5 :: RNil
+
+  println(list5.mergeSort(Ordering[Int]))
 }
